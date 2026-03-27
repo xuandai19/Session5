eb940f3638813d5315b04a20190ac3a78e58b554
Xuân Đại <daik5xuanvinh@gmail.com>
2026-03-28 00:30:20 +0700

    Update and rename console_15.sql to Tim_san_pham_ban_chay_nhat_va_ss_doanh_thu_giua_cac_nhom.sql

---
+++ Tim_san_pham_ban_chay_nhat_va_ss_doanh_thu_giua_cac_nhom.sql
@@ -0,0 +1,51 @@
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(100) NOT NULL,
+    category VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    product_id INT REFERENCES products(product_id),
+    quantity INT NOT NULL,
+    total_price NUMERIC(10, 2) NOT NULL
+);
+
+INSERT INTO products (product_id, product_name, category)
+VALUES (1, 'Laptop Dell', 'Electronics'),
+    (2, 'IPhone 15', 'Electronics'),
+    (3, 'Bàn học gỗ', 'Furniture'),
+    (4, 'Ghế xoay', 'Furniture');
+
+INSERT INTO orders (order_id, product_id, quantity, total_price)
+VALUES (101, 1, 2, 2200),
+    (102, 2, 3, 3300),
+    (103, 3, 5, 2500),
+    (104, 4, 4, 1600),
+    (105, 1, 1, 1100);
+
+SELECT p.category, SUM(o.total_price) total_sales, SUM(o.quantity) total_quantity
+FROM products p JOIN orders o ON p.product_id = o.product_id
+GROUP BY p.category
+HAVING SUM(o.total_price) > 2000
+ORDER BY total_sales DESC;
+
+SELECT p.product_name, sub.total_revenue
+FROM products p JOIN (
+    SELECT product_id, SUM(total_price) total_revenue
+    FROM orders
+    GROUP BY product_id
+) sub ON p.product_id = sub.product_id
+WHERE sub.total_revenue = (
+    SELECT MAX(total_per_product)
+    FROM (
+        SELECT SUM(total_price) total_per_product
+        FROM orders
+        GROUP BY product_id
+    ) max_revenue
+);
+
+SELECT p.category, SUM(o.total_price) total_sales
+FROM products p JOIN orders o ON p.product_id = o.product_id
+GROUP BY p.category
+ORDER BY total_sales DESC;
